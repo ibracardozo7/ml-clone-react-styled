15e98a4ae783e68fda8449eea3868b0ec23d3cf2
ibracardozo7 <ibracardozo15@gmail.com>
2023-02-03 17:28:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import Layout from "./components/Layout/Layout"
+function App() {
+
+  return (
+    <>
+      <Layout />
+      <h1>Mercado libre clone</h1>
+      <GlobalStyles />
+    </>
+  )
+}
+
+export default App
